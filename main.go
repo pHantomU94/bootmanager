665801d031f8bc98ed225f0203d40fe9bea4dcb1
pHantomU94 <dzzxhushengjie@126.com>
2021-02-20 11:46:36 +0800

    change log pattern

--- main.go
+++ main.go
@@ -2,26 +2,19 @@ package main
 
 import (
 	"bootmanager/cmd"
-	"fmt"
-
-	"net/http"
 	_ "net/http/pprof"
 
-	"github.com/sirupsen/logrus"
+	// "github.com/sirupsen/logrus"
 )
 
-func init() {
+// func init() {
 
-	logrus.SetFormatter(&logrus.TextFormatter{
-		FullTimestamp: true,
-	})
-	logrus.SetReportCaller(true)
-}
+// 	// logrus.SetFormatter(&logrus.TextFormatter{
+// 	// 	FullTimestamp: true,
+// 	// })
+// 	logrus.SetReportCaller(true)
+// }
 
 func main() {
-	logrus.Info("start")
-	go func() {
-		fmt.Println(http.ListenAndServe("localhost:6060", nil))
-	}()
 	cmd.Execute()
 }
