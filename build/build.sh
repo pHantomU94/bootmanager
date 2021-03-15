8ceb5c2819c67a54aa0f4057f6789e81abd384cd
pHantomU94 <dzzxhushengjie@126.com>
2021-03-15 11:49:27 +0800

    update build file

--- build/build.sh
+++ build/build.sh
@@ -1,7 +1,17 @@
 #!/bin/bash
 
+echo "#############################################################"
+echo "#                 bootmanager install                       #"
+echo "# author: hsj                                               #"
+echo "#############################################################"
+echo ""
+
+
+red='\033[0;31m'
+green="\033[0;32m"
+
 if [ ! x"${USER}" = x"root" ];then
-    echo "Please rerun `basename $0` as root. sudo ./build.sh" 
+    echo -e ${red}"Please rerun `basename $0` as root. sudo ./build.sh" 
     exit 1
 fi
 
@@ -21,3 +31,5 @@ if [ ! -d $CONFIGURE_DIR ]; then
 fi
 
 cp $CONFIGURE_FILE $CONFIGURE_DIR
+
+echo -e ${green}Success!
